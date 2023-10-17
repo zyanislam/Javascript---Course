c4f962482c5e80b7076c14f0f63f3bc7a5712978
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-10-17 22:58:41 +0600

    Code cleanup

---
+++ arrays.js
@@ -0,0 +1,8 @@
+const itemArray = [];
+
+function addItem() {
+    const inputF = document.querySelector('.inputField');
+    let item = inputF.value;
+    itemArray.push(item);
+    console.log(itemArray);
+}
