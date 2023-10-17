5fe52edd3ddd439b7dbf3200bb24c3b50ac15ef4
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-10-17 23:02:12 +0600

    Minor Update

--- arrays.js
+++ arrays.js
@@ -2,7 +2,9 @@ const itemArray = [];
 
 function addItem() {
     const inputF = document.querySelector('.inputField');
+
     let item = inputF.value;
     itemArray.push(item);
     console.log(itemArray);
+    inputF.value = '';
 }
