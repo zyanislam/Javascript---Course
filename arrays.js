6b08cce2673469efab66a176fbba441eaa76eb18
Sheikh Fahad Al Islam <sheikh.zyanislam@gmail.com>
2023-10-18 11:44:02 +0600

    Update To-Do List

--- arrays.js
+++ arrays.js
@@ -1,10 +1,31 @@
-const itemArray = [];
+const itemArray1 = [];
+const itemArray2 = [];
+let i = 0;
 
-function addItem() {
-    const inputF = document.querySelector('.inputField');
+function addItem1() {
+    const inputF = document.querySelector('.inputField1');
 
     let item = inputF.value;
-    itemArray.push(item);
-    console.log(itemArray);
+    itemArray1.push(item);
+    console.log(itemArray1);
     inputF.value = '';
+}
+
+function addItem2() {
+    const inputF = document.querySelector('.inputField2');
+
+    let item = inputF.value;
+    itemArray2.push(item);
+    console.log(itemArray2);
+    inputF.value = '';
+}
+
+function showItem(){
+    const items = document.querySelector('.itemContainer');
+    while(i != itemArray2.length){
+        console.log(itemArray2);
+        items.innerHTML = `Item No ${i+1}: ${itemArray2[i]}`;
+        i++;
+    }
+
 }
